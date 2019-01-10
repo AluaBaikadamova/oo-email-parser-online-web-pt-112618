09f952959ccba8f75d2ac5886e0b0add99bb638e
Alua Baikadamova <abaikad00@gmail.com>
2019-01-10 03:30:45 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ def initialize(emails)
     email=[]
     email=emails.split(/[,\s]/)
     email=email.uniq
-    email.reject { |e| e.to_s.empty? }
+    email.reject { |e| e.blank? }
     return email
   end
 end
